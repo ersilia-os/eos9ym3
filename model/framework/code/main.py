7ffdc58d7593d2c05b02a6deacbc8b541261e3d6
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2025-01-05 10:10:47 +0100

    Added columns

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -56,7 +56,7 @@ assert input_len == output_len
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["value"]) # header
+    writer.writerow(["logp"]) # header
     for o in outputs:
         writer.writerow([o])
 
